44cfd6e6e9bb466d0196d4c08c860bff262a92d7
Emil Bruun Larsen <Emil.bruun.larsen@gmail.com>
2020-02-25 09:31:16 +0100

    Alpha beta version 32

--- js/script.js
+++ js/script.js
@@ -1,2 +1,69 @@
-var rellax = new Rellax('.rellax');
+let rellax = new Rellax('.rellax');
+
+
+/* ----------menu------------ */
+
+
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+    let getMenu = document.querySelector("#menu-expand");
+    let getMenuUl = document.querySelector("#menu-expand ul");
+    let getMenuLinks = document.querySelectorAll("#menu-expand a");
+    
+    if (toggleNavStatus === false) {
+        getMenuUl.style.display = "block";
+        getMenu.style.height = "91vh";
+        
+        let arrayLength = getMenuLinks.length;
+        for (let i = 0; i < arrayLength; i++) {
+            getMenuLinks[i].style.opacity = "1";
+        }
+        
+        toggleNavStatus = true;
+    } 
+    
+    else if (toggleNavStatus === true) {
+        getMenu.style.height = "0vh";
+        
+        let arrayLength = getMenuLinks.length;
+        for (let i = 0; i < arrayLength; i++) {
+            getMenuLinks[i].style.opacity = "0";
+        }
+        
+        getMenuUl.style.display = "none";
+        
+        toggleNavStatus = false;
+    } 
+}
+
+let toggleLink = function() {
+    
+    let getMenu = document.querySelector("#menu-expand");
+    let getMenuUl = document.querySelector("#menu-expand ul");
+    let getMenuLinks = document.querySelectorAll("#menu-expand a");
+    
+    if (toggleNavStatus === true) {
+        getMenu.style.height = "0vh";
+        
+        let arrayLength = getMenuLinks.length;
+        for (let i = 0; i < arrayLength; i++) {
+            getMenuLinks[i].style.opacity = "0";
+        }
+        
+        getMenuUl.style.display = "none";
+        
+        toggleNavStatus = false;
+    } 
+}
+
+
+
+
+
+
+
+
+
+
 
